13bffa124a0c8df2208c91afe61a3be76277ab41
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-06-11 15:46:00 +0100

    Complete single park floor class

--- parking_floor.py
+++ parking_floor.py
@@ -1,13 +1,24 @@
-import vehicles
+from vehicles import *
 
-class ParkingFloor:
+# 2
+class Parking_floor:
+    '''
+    This class represents a floor in the parking garage and is responsible for managing the parking spots on that floor.
+    '''
     def __init__(self, spot_count):
-        self._spots = [0] * spot_count
+        '''
+        _spots: a list representing the availability of each spot on the floor, zero means spots are vacant.
+        _vehicle_map: a dictionary mapping vehicles to their parking spot range on the floor
+        '''
+        self._spots = [0] * spot_count          # 车位数
         self._vehicle_map = {}
 
     def park_vehicle(self, vehicle):
-        size = vehicles.Vehicle.get_size
-        l, r = 0, 0
+        '''
+        park a vehicle on this floor
+        '''
+        size = vehicle.get_vehicle_size()
+        l, r = 0, 0     # set 2 scope pointers for searching a parking spot
         while r < len(self._spots):
             if self._spots[r] != 0:
                 l = r + 1
@@ -17,17 +28,27 @@ class ParkingFloor:
                     self._spots[k] = 1
                 self._vehicle_map[vehicle] = [l, r]
                 return True
+            
             r += 1
         return False
 
     def remove_vehicle(self, vehicle):
+        '''
+        remove a vehicle from the floor
+        '''
         start, end = self._vehicle_map[vehicle]
         for i in range(start, end + 1):
-            self._spots[i] = 0
+            self._spots[i] = 0      # change 1 to 0
         del self._vehicle_map[vehicle]
 
     def get_parking_spots(self):
+        '''
+        returns the current availability of parking spots on the floor.        
+        '''
         return self._spots
 
     def get_vehicle_spots(self, vehicle):
+        '''
+        returns the spot range occupied by a specific vehicle
+        '''
         return self._vehicle_map.get(vehicle)
